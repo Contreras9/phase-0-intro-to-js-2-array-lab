a7cc7cd052bf1f1ca52441486376e5b2adee60e4
Hamzah Contreras <Hamzah_36@outlook.com>
2021-10-15 17:09:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats];
+    return [...newCats, name];
+}
+
+function prependCat(name) {
+    const newCats = [...cats];
+    return [name, ...newCats]
+}
+
+function removeLastCat() {
+    const copyCats = cats.slice(0, cats.length -1);
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
