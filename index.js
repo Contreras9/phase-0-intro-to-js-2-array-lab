6d709dd732d11b0a2ffd9d4f45c1c1429217e4cb
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-12 10:35:42 -0600

    commit tree addc136f61db05f23c6d3768c05bc9c2d7883482

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-// Write your solution here!
+// // Write your solution here!
 
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    return cats.push(name);
+    const newCats = cats.push(name)
+    return newCats
 }
 
-function destructivelyPrependCat(name) {
-    return cats.unshift(name);
+const destructivelyPrependCat = function(name) {
+    const newCats = cats.unshift(name)
+    return newCats
 }
 
 function destructivelyRemoveLastCat() {
-    return cats.pop();
+    const newCats = cats.pop()
+    return newCats
 }
 
-function destructivelyRemoveFirstCat() {
-    return cats.shift();
+const destructivelyRemoveFirstCat = function() {
+    const newCats = cats.shift()
+    return newCats
 }
 
 function appendCat(name) {
-    const newCats = [...cats];
-    return [...newCats, name];
+    const copyCats = [...cats, `${name}`]
+    return copyCats
+
 }
 
-function prependCat(name) {
-    const newCats = [...cats];
-    return [name, ...newCats]
+const prependCat = function(name) {
+    const copyCats = [`${name}`, ...cats]
+    return copyCats
+
 }
 
 function removeLastCat() {
-    const copyCats = cats.slice(0, cats.length -1);
-    return copyCats;
+    const copyCats = [...cats].slice(0, cats.length -1)
+    return copyCats
+
 }
 
-function removeFirstCat() {
-    const copyCats = cats.slice(1);
-    return copyCats;
+const removeFirstCat = function() {
+    const copyCats = [...cats].slice(1)
+    return copyCats
 }
